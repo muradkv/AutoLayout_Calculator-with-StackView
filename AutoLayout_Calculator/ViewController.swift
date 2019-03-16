7db6b43167ee08b1c3e97074509e329c1dbdaec4
murad <muradkv@gmail.com>
2019-03-16 21:34:04 +0300

    Initial commit

---
+++ AutoLayout_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout_Calculator
+//
+//  Created by murad on 16/03/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
